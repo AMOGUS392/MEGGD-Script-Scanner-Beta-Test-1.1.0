efa332651fb1b91ed0aa00e36b43b1b73b3d27fd
AMOGUS392 <alimtlehuray85@gmail.com>
2026-03-30 12:15:43 +0300

    Fix function name in WarningChecker.lua

--- Warning/WarningChecker.lua
+++ Warning/WarningChecker.lua
@@ -13,7 +13,7 @@ if not m then return end
 local f={}
 if not decompile then table.insert(f,"decompile") end
 if not getscripthash then table.insert(f,"getscripthash") end
-if not getscripts then table.insert(f,"getscripts") end
+if not getscript then table.insert(f,"getscripts") end
 if not getnilinstances then table.insert(f,"getnilinstances") end
 local y=20
 for i,v in ipairs(f) do
